ce4cc54cdbe6475aa5cca368f68eacfbf4348190
AlishaKausheen <alishakausheen@gmail.com>
2023-10-03 09:52:08 -0700

    jEasy function1 added

---
+++ jEasy.js
@@ -0,0 +1,14 @@
+class ElementCollection extends Array{
+    ready(cb) {
+        const isReady = this.some(e => {
+            return e.readyState! = null && e.readyState! = "loading"
+        })
+        if (isReady) {
+            cb()
+        }
+        else {
+            this.on("DOMContentLoaded",cb)
+        }
+        return this
+    }
+}
