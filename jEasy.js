960701dab90e610ba5bfd18a26478173e83d436d
AlishaKausheen <alishakausheen@gmail.com>
2023-10-10 00:43:03 -0700

    ready function

--- jEasy.js
+++ jEasy.js
@@ -1,5 +1,21 @@
 //creating a class that encapsulate the array functionality
-class ElementCollection extends Array {}
+class ElementCollection extends Array {
+  ready(cb) {
+    // call back as param to know when it is ready
+    // we have to call ready whenever we are passing
+    const isReady = this.some((e) => {
+      // if some of the element in the array matches the selector then we know that we are ready
+
+      return e.readyState != null && e.readyState != `loading`; // we have some type of readystate and not loading
+    });
+    if (isReady) {
+      // if the page is already loaded
+      cb(); // call back function
+    } else {
+      document.addEventListener(`DOMContentLoaded`, cb); // if page is finished loading
+    }
+  }
+}
 function $(param) {
   // in jEasy this $ sign is returning a special type of array
   if (typeof param === `string` || param instanceof String) {
